7ffdde60a5c770424c2ef25e4cde6aff3fa79784
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-01-07 16:21:05 -0500

    attempt again. Error may be from install script

--- src/scripts/install-nvm.sh
+++ src/scripts/install-nvm.sh
@@ -1,6 +1,6 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 echo 'export NVM_DIR="$HOME/.nvm"' >> $BASH_ENV
-echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"" >> $BASH_ENV
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $BASH_ENV
 source $BASH_ENV
 if [ "$NODE_PARAM_LTS" = "1" ]; then
     nvm install --lts
