242833fc8f56f527760e14dc9ebe77a87667f72b
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-02-19 04:43:39 -0500

    Only install nvm when needed. [semver:patch] (#72)

--- src/scripts/install-nvm.sh
+++ src/scripts/install-nvm.sh
@@ -1,7 +1,13 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-echo 'export NVM_DIR="$HOME/.nvm"' >> $BASH_ENV
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $BASH_ENV
-source $BASH_ENV
+# Only install nvm if it's not already installed
+if command -v nvm &> /dev/null; then
+    echo "nvm is already installed. Skipping nvm install.";
+else
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash;
+    echo 'export NVM_DIR="$HOME/.nvm"' >> $BASH_ENV;
+    echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $BASH_ENV;
+    source $BASH_ENV;
+fi
+
 if [ "$NODE_PARAM_LTS" = "1" ]; then
     nvm install --lts
 else
