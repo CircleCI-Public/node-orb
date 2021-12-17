f14b3f5f3c7a4b523b0d8abc2404f6ce5c14471c
Jaryt Bustard <jarytbustard@gmail.com>
2021-12-17 12:59:41 -0500

    feat: make lts the default rather than latest

--- src/scripts/install-nvm.sh
+++ src/scripts/install-nvm.sh
@@ -11,9 +11,12 @@ else
     source "$BASH_ENV";
 fi
 
-if [ "$NODE_PARAM_VERSION" = "lts" ]; then
-    nvm install --lts
-    nvm alias default lts/*
+# See: https://github.com/nvm-sh/nvm#usage
+if [ "$NODE_PARAM_VERSION" = "latest" ]; then
+    # When no version is specified we default to the latest version of Node
+    NODE_ORB_INSTALL_VERSION=$(nvm ls-remote | tail -n1 | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+')
+    nvm install "$NODE_ORB_INSTALL_VERSION" # aka nvm install node. We're being explicit here.
+    nvm alias default "$NODE_ORB_INSTALL_VERSION"
     elif [ -n "$NODE_PARAM_VERSION" ]; then
     nvm install "$NODE_PARAM_VERSION"
     nvm alias default "$NODE_PARAM_VERSION"
@@ -22,9 +25,8 @@ if [ "$NODE_PARAM_VERSION" = "lts" ]; then
     nvm install "$NVMRC_SPECIFIED_VERSION"
     nvm alias default "$NVMRC_SPECIFIED_VERSION"
 else
-    NODE_ORB_INSTALL_VERSION=$(nvm ls-remote | tail -n1 | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+')
-    nvm install "node"
-    nvm alias default "$NODE_ORB_INSTALL_VERSION"
+    nvm install --lts
+    nvm alias default lts/*
 fi
 
 echo 'nvm use default &>/dev/null' >> "$BASH_ENV"
