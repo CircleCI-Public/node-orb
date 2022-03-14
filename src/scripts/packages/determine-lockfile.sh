d2bd87297072bd738405d400259fa4fa1c074baf
Jaryt Bustard <jarytbustard@gmail.com>
2022-03-14 15:23:52 -0400

    fix: use copy instead of link

--- src/scripts/packages/determine-lockfile.sh
+++ src/scripts/packages/determine-lockfile.sh
@@ -1,12 +1,12 @@
 # Link corresponding lock file to a temporary file used by cache commands
 if [ -f "package-lock.json" ]; then
     echo "Found package-lock.json file, assuming lockfile"
-    ln -s package-lock.json /tmp/node-project-lockfile
+    cp package-lock.json /tmp/node-project-lockfile
 elif [ -f "npm-shrinkwrap.json" ]; then
     echo "Found npm-shrinkwrap.json file, assuming lockfile"
-    ln -s npm-shrinkwrap.json /tmp/node-project-lockfile
+    cp npm-shrinkwrap.json /tmp/node-project-lockfile
 elif [ -f "yarn.lock" ]; then
     echo "Found yarn.lock file, assuming lockfile"
-    ln -s yarn.lock /tmp/node-project-lockfile
+    cp yarn.lock /tmp/node-project-lockfile
 fi
-ln -s package.json /tmp/node-project-package.json
+cp package.json /tmp/node-project-package.json
