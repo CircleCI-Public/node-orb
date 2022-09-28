589b11670efb218c5c67d23ced3aaf155adf7a69
Gerkin <alexandre.j.p.germain@gmail.com>
2022-09-28 23:10:23 +0200

    fix: support cache computation on windows (#157)
    * fix: support cache computation on windows
    
    On windows runners, the cache keys fails to be computed because of the `checksum` function that can't find the file in /tmp. Indeed, it seems that bash `/tmp` is not the runner's `C:\tmp` directory.
    
    See https://app.circleci.com/pipelines/github/KnodesCommunity/typedoc-plugins/693/workflows/897cb674-230e-46ae-99f5-b10d93f4b723/jobs/3872/parallel-runs/0/steps/0-106 for an example of failed step
    
    * Update determine-lockfile.sh
    
    * Add quote to variable expansion
    
    * remove trailing backslash
    
    * ci: add tests for the windows executor
    
    Co-authored-by: Eric Ribeiro <ericribeiro@outlook.com.br>

--- src/scripts/packages/determine-lockfile.sh
+++ src/scripts/packages/determine-lockfile.sh
@@ -1,12 +1,18 @@
+TARGET_DIR="/tmp"
+if [ -n "$HOMEDRIVE" ]; then
+    TARGET_DIR="$HOMEDRIVE\\tmp"
+fi
+
 # Link corresponding lock file to a temporary file used by cache commands
 if [ -f "package-lock.json" ]; then
     echo "Found package-lock.json file, assuming lockfile"
-    cp package-lock.json /tmp/node-project-lockfile
+    cp package-lock.json $TARGET_DIR/node-project-lockfile
 elif [ -f "npm-shrinkwrap.json" ]; then
     echo "Found npm-shrinkwrap.json file, assuming lockfile"
-    cp npm-shrinkwrap.json /tmp/node-project-lockfile
+    cp npm-shrinkwrap.json $TARGET_DIR/node-project-lockfile
 elif [ -f "yarn.lock" ]; then
     echo "Found yarn.lock file, assuming lockfile"
-    cp yarn.lock /tmp/node-project-lockfile
+    cp yarn.lock $TARGET_DIR/node-project-lockfile
 fi
-cp package.json /tmp/node-project-package.json
+
+cp package.json $TARGET_DIR/node-project-package.json
