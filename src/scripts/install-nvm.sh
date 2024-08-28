a961cb54d7fdd925bc367719b2ae3f86e6031583
David Sanders <dsanders11@ucsbalum.com>
2024-08-28 11:00:50 -0400

    fix!: do not fall back to source builds of Node.js (#185)
    Co-authored-by: marboledacci <mateo.arboleda@circleci.com>

--- src/scripts/install-nvm.sh
+++ src/scripts/install-nvm.sh
@@ -18,21 +18,21 @@ fi
 if [ "$NODE_PARAM_VERSION" = "latest" ]; then
     # When no version is specified we default to the latest version of Node
     NODE_ORB_INSTALL_VERSION=$(nvm ls-remote | tail -n1 | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+')
-    nvm install "$NODE_ORB_INSTALL_VERSION" # aka nvm install node. We're being explicit here.
+    nvm install -b "$NODE_ORB_INSTALL_VERSION" # aka nvm install node. We're being explicit here.
     nvm alias default "$NODE_ORB_INSTALL_VERSION"
 elif [ -n "$NODE_PARAM_VERSION" ] && [ "$NODE_PARAM_VERSION" != "lts" ]; then
-    nvm install "$NODE_PARAM_VERSION"
+    nvm install -b "$NODE_PARAM_VERSION"
     nvm alias default "$NODE_PARAM_VERSION"
 elif [ -f ".nvmrc" ]; then
     NVMRC_SPECIFIED_VERSION=$(<.nvmrc)
-    nvm install "$NVMRC_SPECIFIED_VERSION"
+    nvm install -b "$NVMRC_SPECIFIED_VERSION"
     nvm alias default "$NVMRC_SPECIFIED_VERSION"
 elif [ -f ".node-version" ]; then
     NVMRC_SPECIFIED_VERSION=$(<.node-version)
     nvm install "$NVMRC_SPECIFIED_VERSION"
     nvm alias default "$NVMRC_SPECIFIED_VERSION"
 else
-    nvm install --lts
+    nvm install -b --lts
     nvm alias default lts/*
 fi
 
