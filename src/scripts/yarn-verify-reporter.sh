6de1402076d852f917450d5a42ac208aaff5f4c7
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-08-05 09:22:52 -0500

    fix berry validation

--- src/scripts/yarn-verify-reporter.sh
+++ src/scripts/yarn-verify-reporter.sh
@@ -7,5 +7,5 @@ MAJOR_VERSION=${YARN_VERSION%%.*}
 if [[ "$MAJOR_VERSION" -eq 1 ]]; then
   yarn list --pattern "${PARAM_PATTERN}" | grep "${PARAM_GREP}" || (echo "${PARAM_ERROR}" && exit 1)
 else
-  yarn info --pattern "${PARAM_PATTERN}" | grep "${PARAM_GREP}" || (echo "${PARAM_ERROR}" && exit 1)
+  yarn info | grep "${PARAM_GREP}" || (echo "${PARAM_ERROR}" && exit 1)
 fi
